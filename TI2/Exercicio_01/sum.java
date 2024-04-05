f6a7ffd432262afca29a2362e00ecd56cc4d9d7b
Heitoralvernaz001 <heitordosestudos@gmail.com>
2024-04-05 02:31:57 -0300

    comit

---
+++ TI2/Exercicio_01/sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc1 = new Scanner(System.in);
+        int x,  y,  sum;
+
+        System.out.print("primeiro num: ");
+        x = sc1.nextInt();
+        System.out.print("segundo num : ");
+        y = sc1.nextInt();
+        sum = x + y;
+        System.out.println("Soma: " + sum);	
+	}
+
+}
